14ba4592ea54699d2a69275b0926549c4e0dbcec
thely <bardbarienne@gmail.com>
2020-11-13 16:13:37 -0500

    further tooltip fixes, adjusting graph order

--- main.js
+++ main.js
@@ -223,11 +223,14 @@ const selectedShapes = cityCircles.selectAll("circles")
     .on('mouseenter', function(event, d) {
         // show tooltip on mouse enter
         // tip.show(d.ct, this);
-        let rect = event.target.ownerSVGElement.getBoundingClientRect();
+        // console.log(event.target.ownerSVGElement.ownerSVGElement);
+        // console.log(event.target);
+        // let rect = event.target.ownerSVGElement.ownerSVGElement.parentElement.getBoundingClientRect();
+        // console.log(rect);
         newTip.style("opacity", 1);
         newTip.html(d.ct)
-            .style("left", (event.clientX - rect.left) + "px")
-            .style("top", (event.clientY - rect.top - 10) + "px");
+            .style("left", (event.clientX) + "px")
+            .style("top", (event.clientY - 10) + "px");
         d3.select(this).attr('fill', "red");
     })
     .on('click', function(event, d) {
