6f6e1b31be1364e197e25dd24bc94ea48839bd03
Jules Becker <becker2430@gmail.com>
2024-10-29 23:08:17 -0400

    serve static files correctly

--- server.js
+++ server.js
@@ -2,15 +2,29 @@ const express = require('express');
 const app = express();
 var path = require('path');
 
+// Properly serve static files with correct MIME types
+app.use('/static', express.static(path.join(__dirname, 'static'), {
+  setHeaders: (res, filePath) => {
+    if (filePath.endsWith('.js')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    }
+    if (filePath.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    }
+    // Remove nosniff header
+    res.removeHeader('X-Content-Type-Options');
+  }
+}));
+
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + '/static/index.html'));
+  res.sendFile(path.join(__dirname, 'static/index.html'));
 });
 
 app.get("/ext", function (req, res) {
   res.sendFile(path.join(__dirname + '/static/json/external.json'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 let server = app.listen(port, function () {
   console.log(`listening on ${port}`);
 });
